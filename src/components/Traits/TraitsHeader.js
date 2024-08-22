5711f1ccd80400faed3ea7914f5ad52d4454b5c3
Ginodavecte <ginojoanroy@hotmail.com>
2024-08-23 01:48:29 +0200

    fuck image

--- src/components/Traits/TraitsHeader.js
+++ src/components/Traits/TraitsHeader.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TopBar from '../../layout/TopBar';
 import { GiSkills } from 'react-icons/gi';
-import ContentPageImage from '../../layout/ContentPageImage'
-import pageImage from './FM-Traits.webp';
+// import ContentPageImage from '../../layout/ContentPageImage'
+// import pageImage from './FM-Traits.webp';
 
 const TraitsHeader = ( { mainClass, introduction } ) => {
   return (
     <div className={`${mainClass}__header`}>
       <TopBar title="Traits" icon={<GiSkills />} />
-      <ContentPageImage imageSrc={pageImage} />
+      {/*<ContentPageImage imageSrc={pageImage} />*/}
       <div className={`${mainClass}__introduction`}>
         <h3>Introductie</h3>
         <p>{introduction}</p>
