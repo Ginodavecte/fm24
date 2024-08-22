dfbb0d2047b5edd4376d6f980ae2bce314216521
Ginodavecte <ginojoanroy@hotmail.com>
2024-08-23 00:35:26 +0200

    code clean up

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -6,16 +6,16 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-      <div className="footer__title">
-        Referenties
-      </div>
-      <div className="footer__content">
-        <ul>
-          <li>
-            <a href="https://fmcalc.com">Calculator</a>
-          </li>
-        </ul>
-      </div>
+        <div className="footer__title">
+          Referenties
+        </div>
+        <div className="footer__content">
+          <ul>
+            <li>
+              <a href="https://fmcalc.com">Calculator</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
